28e2dccbe6842ee72e1d910c0a9f3b6ac2716fd5
Benjamin Piggin <56440821+bpiggin@users.noreply.github.com>
2019-10-23 18:07:02 +0100

    Add files via upload

---
+++ javascript/MaxArraySequenceSum.js
@@ -0,0 +1,20 @@
+  var maxSequence = function(arr) {
+    let result = 0;
+    let largestSum = 0;
+    for (var i=1; i<arr.length; i++) {
+      largestSum = findLargestSubArraySum(i, arr);
+      if (+largestSum > result) result = largestSum;
+    }
+    return result;
+  }
+
+  function findLargestSubArraySum(lengthOfSubArray, array) {
+    let largestSum = 0;
+    let result = 0;
+    for (var i=0; i + lengthOfSubArray < array.length; i++) {
+      result = array.slice(i, i + lengthOfSubArray).reduce((a,b) => +a + +b, 0);
+      if (+result > largestSum) largestSum = result;
+    }
+    return largestSum;
+  }
+  console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
